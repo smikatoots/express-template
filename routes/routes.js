cd69bc5d931c8130576a1e455ae8e3f5e8d3d6e4
blerude <benjamin.lerude@yale.edu>
2017-07-14 15:03:01 -0700

    Routes

--- routes/routes.js
+++ routes/routes.js
@@ -109,85 +109,11 @@ io.on('connection', function(socket) {
       // emit non-positive event
       socket.emit("negativeMessage")
     }
-
-<<<<<<< Updated upstream
-
-=======
-})
-
-//////////////////// LANDING PAGE WITH OPTIONS FOR SIGNUP AND LOGIN ////////////////////////////////
-// Users who are not logged in can see these routes
-
-router.get('/', function(req, res, next) {
-  res.render('landing');
-});
-
-///////////////////////////// THE WALL /////////////////////////////
-
-router.use(function(req, res, next){
-  if (!req.user) {
-    res.redirect('/login');
-  } else {
-    return next();
-  }
-});
-
-//////////////////////////////// PRIVATE ROUTES ////////////////////////////////
-// Only logged in users can see these routes
-
-// home page with threads
-router.get('/user', function(req, res) {
-  var threads = [];
-  threads.push(Thread.find({participant2: req.user._id}).populate("participant1"));
-  threads.push(Thread.find({participant1: req.user._id}).populate("participant2"));
-  Promise.all(threads)
-  .then(function(threads) {
-    res.render('user', {
-      user: req.user,
-      received: threads[0],
-      sent: threads[1]
-    });
-  })
-});
-
-router.post('/messages/:friendid', function(req, res) {
-  var friendid = req.params.friendid;
-  var content = req.body.content;
-  var createdAt = new Date();
-  var anonymousSender = req.body.anonymous
-  if (filter.clean(content).includes('~')) {
-    // emit dirty event
-    socket.emit('dirtyMessage')
-  }
-  if (sentiment(content).score < 5) {
-    // emit non-positive event
-  }
-
-  new Message({
-    sender: req.user._id,
-    reciever: friendid,
-    content: content,
-    createdAt: createdAt,
-    read: false
-  }).save(function(err, message) {
-    if (err) {
-      console.log("Error while sending message", err)
-    } else {
-      new Thread({
-        participant1: req.user._id,
-        anonymousSender: anonymousSender,
-        participant2: friendid,
-        firstMessage: message,
-        replies: []
-      }).save(function(err) {
-        if (err) {
-          console.log("Error while creating thread", err)
-        } else {
-          // emit new message event
-        }
-      })
-    }
->>>>>>> Stashed changes
+    Thread.findById(threadid, function(err, thread) {
+      if (err) {
+        console.log("Could not identify")
+      }
+    })
   })
 })
 
